16a8e9b56602bc48fc76245593a51b657c1de6b8
maria-mamede <maria.mamede@mercadolivre.com>
2022-07-06 12:37:08 -0300

    endpoints

--- src/main/java/com/dh/meli/desafioSpring/controller/ProdutoController.java
+++ src/main/java/com/dh/meli/desafioSpring/controller/ProdutoController.java
@@ -1,12 +1,12 @@
 package com.dh.meli.desafioSpring.controller;
 
 import com.dh.meli.desafioSpring.dto.ProdutoDto;
+import com.dh.meli.desafioSpring.model.Produto;
 import com.dh.meli.desafioSpring.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,16 +17,21 @@ public class ProdutoController {
     @Autowired
     private ProdutoService service;
 
-//
-//    public ResponseEntity<ProdutoDto>(){
-//        List<ProdutoDto> lista = service.get
-//        return
-//    }
+    @PostMapping("/insert-articles-request")
+    @ResponseStatus(HttpStatus.OK)
+    public void adicionarProduto(@RequestBody Produto produto){
 
-    @GetMapping
-    public ResponseEntity<List<ProdutoDto>> getAllProdutosDisponiveis() {
+    }
 
+    @GetMapping("/articles")
+    public ResponseEntity<ProdutoDto> getAllCategoria(@RequestParam String categoria){
         return null;
     }
+
+    @GetMapping("/articles")
+    public ResponseEntity<List<ProdutoDto>> getAllProdutosDisponiveis() {
+        return null;
+    }
+
 
 }
