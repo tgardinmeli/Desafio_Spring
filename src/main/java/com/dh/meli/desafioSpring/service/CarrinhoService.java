6f9034254f69b1a839733588c3b06a6a39404b9e
Danielen <danielen.cestari@mercadolivre.com>
2022-07-07 16:03:31 -0300

    req 08 - implementa carrinho funcionando parcialmente

--- src/main/java/com/dh/meli/desafioSpring/service/CarrinhoService.java
+++ src/main/java/com/dh/meli/desafioSpring/service/CarrinhoService.java
@@ -3,13 +3,14 @@ package com.dh.meli.desafioSpring.service;
 import com.dh.meli.desafioSpring.dto.RequestProdutoDto;
 import com.dh.meli.desafioSpring.dto.TicketDto;
 import com.dh.meli.desafioSpring.model.CarrinhoCompras;
+import com.dh.meli.desafioSpring.model.Produto;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public interface CarrinhoService {
-    HashMap verificarProduto(List<RequestProdutoDto> articlesPurchaseRequest);
+    HashMap <Long, Produto>verificarProduto(List<RequestProdutoDto> articlesPurchaseRequest);
     TicketDto processarCompra(List<RequestProdutoDto> articlesPurchaseRequest);
 
 }
